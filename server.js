b61c007722b317ee5fadfd390536160fb1f3ed74
zachary <zpyingling9559@gmail.com>
2020-03-28 15:35:27 -0400

    finishing the basic server.js

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const PORT = process.env.PORT || 8080;
+
+const app = express();
+
+app.use(express.static("public"));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Set Handlebars.
+var exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+var routes = require("./controllers/burgers_controller.js");
+
+app.use(routes);
+
+app.listen(PORT, () => {
+  console.log("Server listening.");
+});
+
+module.exports = app;
