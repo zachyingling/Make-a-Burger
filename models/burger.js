462bb39c5b318d314952b6c4bd9bdd9df8b43a70
zachary <zpyingling9559@gmail.com>
2020-03-28 16:58:44 -0400

    finishing the burger model

---
+++ models/burger.js
@@ -0,0 +1,21 @@
+const orm = require("../config/orm.js");
+
+const burger = {
+  all: cb => {
+    orm.selectAll("burgers", res => {
+      cb(res);
+    });
+  },
+  insert: (cols, vals, cb) => {
+    orm.insertOne("burgers", cols, vals, res => {
+      cb(res);
+    });
+  },
+  update: (objColVals, condition, cb) => {
+    orm.updateOne("burgers", objColVals, condition, res => {
+      cb(res);
+    });
+  }
+};
+
+module.exports = burger;
