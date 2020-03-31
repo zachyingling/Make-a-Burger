cefd063e775600bde218700f22ddf9cc454bba27
zachary <zpyingling9559@gmail.com>
2020-03-31 18:41:25 -0400

    Adding jawsdb fix

--- config/connection.js
+++ config/connection.js
@@ -1,7 +1,7 @@
 const mysql = require("mysql");
-const connection;
+let connection;
 
-if(process.env.JAWSDB_URL){
+if (process.env.JAWSDB_URL) {
   connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
   connection = mysql.createConnection({
--- db/schema.sql
+++ db/schema.sql
@@ -5,9 +5,10 @@ CREATE TABLE burgers
 (
   id INTEGER(11)
   AUTO_INCREMENT NOT NULL,
-  burger_name VARCHAR
+	burger_name VARCHAR
   (100) NOT NULL,
-  devoured BOOLEAN NOT NULL,
-  PRIMARY KEY
+	devoured BOOLEAN NOT NULL,
+    createdAt TIMESTAMP NOT NULL,
+	PRIMARY KEY
   (id)
 );
